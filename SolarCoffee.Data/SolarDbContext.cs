3880adefca6a72d933d7a171e7c2c64e6f38177d
Arnfinn Larsen <finnarn.larsen@gmail.com>
2021-06-23 14:57:15 +0200

    added tables and db connection string

--- SolarCoffee.Data/SolarDbContext.cs
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SolarCoffee.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace SolarCoffee.Data
 {
-    class SolarDbContext
+    public class SolarDbContext : IdentityDbContext
     {
+        public SolarDbContext(DbContextOptions options) :base(options)  {}
+
+        public virtual DbSet<Customer> Customers{ get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddresses { get; set; }
 
     }
 }
